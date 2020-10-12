9b84f22db3d8f56e63356d99304277d7f1858dd3
Anna Stanek <aniastanekk@gmail.com>
2020-10-12 14:38:42 +0200

    Create first component

---
+++ src/pages/SignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignIn({ username, password }) {
+  return (
+    <div>
+      To jest komponent logowania
+      <p>{username}</p>
+      <p>{password}</p>
+    </div>
+  );
+}
+
+export default SignIn;
