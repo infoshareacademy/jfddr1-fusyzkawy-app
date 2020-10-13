8171dd622be7275f361161065730d27af38eab88
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-10-13 23:25:48 +0200

    Navigation template WIP

--- src/StructureSample/templates/Navigation.js
+++ src/StructureSample/templates/Navigation.js
@@ -3,3 +3,23 @@
 //Image -> person avatar
 //Username
 //Button -> open UserInformation.js
+import React from "react";
+import Button from "../atoms/Button";
+import UserInformation from "../molecules/UserInformation";
+
+function Navigation({ style, text, url }) {
+  return (
+    <div style={style}>
+      <Button />
+      <nav>
+        <a href={url}>Tasks</a>
+        <a href={url}>Timer</a>
+        <a href={url}>Calendar</a>
+        <a href={url}>Users</a>
+      </nav>
+      <UserInformation />
+    </div>
+  );
+}
+
+export default Navigation;
