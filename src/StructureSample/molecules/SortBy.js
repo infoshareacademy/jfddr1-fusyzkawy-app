f4c254f379837145146e41bb0f7af9f60571cf76
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-10-13 21:14:02 +0200

    SortBy molecule WIP

--- src/StructureSample/molecules/SortBy.js
+++ src/StructureSample/molecules/SortBy.js
@@ -1,2 +1,18 @@
 //h3 -> Sort by...
 //Radio input -> options for sorting
+import React from "react";
+import RadioInput from "../atoms/RadioInput";
+
+function SortBy({ style, text }) {
+  return (
+    <div style={style}>
+      <h3>Sort by</h3>
+      <RadioInput text="" style={{}} isChecked="" />
+      <RadioInput text="" style={{}} isChecked="" />
+      <RadioInput text="" style={{}} isChecked="" />
+      <RadioInput text="" style={{}} isChecked="" />
+    </div>
+  );
+}
+
+export default SortBy;
