ca679a4c6ee7d7ecc867d31729ac5f0faae746ba
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-10-13 20:30:19 +0200

    date and radioinput atoms added

--- src/StructureSample/atoms/Date.js
+++ src/StructureSample/atoms/Date.js
@@ -1,1 +1,12 @@
 //use some library to create data format and checked current data?
+import React from "react";
+
+function Date({ text, style }) {
+  return (
+    <div style={style}>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default Date;
--- src/StructureSample/atoms/RadioInput.js
+++ src/StructureSample/atoms/RadioInput.js
@@ -1,1 +1,13 @@
 //Radio input
+import React from "react";
+
+function RadioInput({ text, style, isChecked }) {
+  return (
+    <div style={style}>
+      <label for={text}>{text}</label>
+      <input type="radio" id={text} value={text} checked={isChecked} />
+    </div>
+  );
+}
+
+export default RadioInput;
