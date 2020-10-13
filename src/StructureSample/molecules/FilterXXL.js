baae82f6f8a3796dbe208f41a3fef1ef2d7d0d08
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-10-13 20:52:29 +0200

    FilterXXL molecule added

--- src/StructureSample/molecules/FilterXXL.js
+++ src/StructureSample/molecules/FilterXXL.js
@@ -6,3 +6,24 @@
 //checkbox -> state of task
 //ckechbox -> priority of task
 //button -> "Apply"
+import React from "react";
+import Input from "../atoms/Input";
+import Checkbox from "../atoms/Checkbox";
+import Button from "../atoms/Button";
+
+function FilterXXL({ style, text }) {
+  return (
+    <div style={style}>
+      <h3>Filter {text}</h3>
+      <p>Sort by</p>
+      <Input />
+      <Checkbox />
+      <Checkbox />
+      <Checkbox />
+      <Checkbox />
+      <Button />
+    </div>
+  );
+}
+
+export default FilterXXL;
