c03e7a4af0a8b5bd350d9a6019f6fd118997bc10
Kuba <jameshopegit@gmail.com>
2020-10-14 21:34:23 +0200

    WIP: Componets ProfileSettings and UserInformation

--- src/StructureSample/molecules/ProfileSettings.js
+++ src/StructureSample/molecules/ProfileSettings.js
@@ -7,3 +7,32 @@
 //phone,  (input)
 //email, (input)
 //Button -> Save Changes
+
+import React from "react";
+import Input from "../atoms/Input";
+
+function ProfileSettings() {
+  return (
+    <div>
+      <h2>Profile Settings</h2>
+      <h3>Profile Details</h3>
+      <p>Name: </p>
+      <Input />
+      <p>User Name: </p>
+      <Input />
+      <p>Date of Birth: </p>
+      <Input />
+      <h3>Contact info</h3>
+      <p>Phone: </p>
+      <Input />
+      <p>Email</p>
+      <Input />
+      {/* dwie linie poniżej tylko na potrzeby chwili */}
+      <br></br>
+      <br></br>
+      <button>Save changes</button>
+    </div>
+  );
+}
+
+export default ProfileSettings;
--- src/StructureSample/molecules/UserInformation.js
+++ src/StructureSample/molecules/UserInformation.js
@@ -3,3 +3,21 @@
 //Profile settings -> open separate component:
 //Change password
 //Button -> "Log out"
+
+import React from "react";
+
+function UserInformation() {
+  return (
+    <div>
+      <img
+        src="https://metabiomedamericas.com/wp-content/uploads/2018/05/facebook-avatar-1024x645.jpg"
+        alt="user photo"
+        className="imgUserInformation"
+      />
+      <p>Username</p>
+      <button>v</button>
+    </div>
+  );
+}
+
+export default UserInformation;
