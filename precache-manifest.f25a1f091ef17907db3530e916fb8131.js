af910199594f064f032fcaf46eea5fc4a05ebd7c
Kuba <jameshopegit@gmail.com>
2020-10-14 13:47:19 +0200

    Updates

---
+++ precache-manifest.f25a1f091ef17907db3530e916fb8131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92dd1b029446fae0d174524a04e1add5",
+    "url": "/infoshareacademy/jfddr1-fusyzkawy-app/index.html"
+  },
+  {
+    "revision": "67d40c9dd938aa5b27cb",
+    "url": "/infoshareacademy/jfddr1-fusyzkawy-app/static/css/main.c9638d7a.chunk.css"
+  },
+  {
+    "revision": "3688e62279dfa21400d8",
+    "url": "/infoshareacademy/jfddr1-fusyzkawy-app/static/js/2.4fc40f29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoshareacademy/jfddr1-fusyzkawy-app/static/js/2.4fc40f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d40c9dd938aa5b27cb",
+    "url": "/infoshareacademy/jfddr1-fusyzkawy-app/static/js/main.e4896051.chunk.js"
+  },
+  {
+    "revision": "630550e4ef06081cebb0",
+    "url": "/infoshareacademy/jfddr1-fusyzkawy-app/static/js/runtime-main.46b581e3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/infoshareacademy/jfddr1-fusyzkawy-app/precache-manifest.f25a1f091ef17907db3530e916fb8131.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infoshareacademy/jfddr1-fusyzkawy-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
