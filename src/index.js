a201765ceab870b62962122292eeb9d5d8cf24d1
Kuba <jameshopegit@gmail.com>
2020-10-16 14:53:38 +0200

    Renamed App component in index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <AppRadek />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
