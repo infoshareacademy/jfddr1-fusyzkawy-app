d583dc4fcb326b2fefd68199e43fa9f937b2991b
Radek Pachulski <radoslaw.pachulski@gmail.com>
2020-10-17 21:37:17 +0200

    11-navigation WIP, delete AppRadek.js, delete redundant code from App.css

--- src/StructureSample/pages/Nav.js
+++ src/StructureSample/pages/Nav.js
@@ -1,22 +1,58 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import UserInformation from "../molecules/UserInformation";
+
+const MainNavBar = styled.div`
+  background-color: var(--basic-navy-blue);
+  grid-area: nav;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 3rem;
+`;
+
+const NavAddTaskBtn = styled.button`
+  background-color: var(--basic-green);
+  border: none;
+  border-radius: 0.5rem;
+  color: var(--basic-light-gray);
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.75rem;
+
+  &:hover {
+    color: var(--gray-1);
+  }
+`;
+
+const LinksWrapper = styled.nav`
+  display: flax;
+`;
+
+const MainNavLink = styled(NavLink)`
+  color: var(--basic-light-gray);
+  text-decoration: none;
+  font-size: 1.5rem;
+  padding: 0.75rem;
+
+  &:hover {
+    background-color: var(--extra-dark-gray);
+  }
+`;
 
 const Nav = () => {
   return (
-    <div className="navHome">
-      <NavLink className="navIcon" to="/">
-        Tasks
-      </NavLink>
-      <NavLink className="navIcon" to="/timer">
-        Timer
-      </NavLink>
-      <NavLink className="navIcon" to="/calendar">
-        Calendar
-      </NavLink>
-      <NavLink className="navIcon" to="/users">
-        Users
-      </NavLink>
-    </div>
+    <MainNavBar>
+      <NavAddTaskBtn>Add Task</NavAddTaskBtn>
+      <LinksWrapper>
+        <MainNavLink to="/">Tasks</MainNavLink>
+        <MainNavLink to="/timer">Timer</MainNavLink>
+        <MainNavLink to="/calendar">Calendar</MainNavLink>
+        <MainNavLink to="/users">Users</MainNavLink>
+      </LinksWrapper>
+      <UserInformation />
+    </MainNavBar>
   );
 };
 
