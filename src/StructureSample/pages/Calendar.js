9d370a1f76e461c08d25bbd81c581a8f4a5cc2f3
Anna Stanek <aniastanekk@gmail.com>
2020-10-19 17:27:18 +0200

    Istall react-big-calendar & moment.js

--- src/StructureSample/pages/Calendar.js
+++ src/StructureSample/pages/Calendar.js
@@ -1,30 +1,61 @@
+// import React from "react";
+// import Month from "../molecules/Month";
+
+// function Filter() {
+//   return <button>Filter</button>;
+// }
+
+// function Calendar({ dateCalendar }) {
+//   return (
+//     <div className="mainHome">
+//       <h2>Calendar</h2>
+//       <div>
+//         <button>Day</button>
+//         <button>Week</button>
+//         <button>Month</button>
+//       </div>
+//       <Filter />
+//       <div className={"displayFLexRow2"}>
+//         <button className={"marginTop"}>Previous </button>
+//         <div className={"widthProcent80"}>
+//           <Month date={dateCalendar} />
+//           {/*zamiennie później z <Day/> & <Week/>*/}
+//         </div>
+//         <button className={"marginTop"}>Next</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Calendar;
+
 import React from "react";
-import Month from "../molecules/Month";
+import * as BigCalendar from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function Filter() {
-  return <button>Filter</button>;
-}
+moment.locale("en-GB");
+BigCalendar.momentLocalizer(moment);
 
-function Calendar({ dateCalendar }) {
+export default function Calendar() {
   return (
-    <div className="mainHome">
-      <h2>Calendar</h2>
-      <div>
-        <button>Day</button>
-        <button>Week</button>
-        <button>Month</button>
-      </div>
-      <Filter />
-      <div className={"displayFLexRow2"}>
-        <button className={"marginTop"}>Previous </button>
-        <div className={"widthProcent80"}>
-          <Month date={dateCalendar} />
-          {/*zamiennie później z <Day/> & <Week/>*/}
-        </div>
-        <button className={"marginTop"}>Next</button>
-      </div>
+    <div style={{ height: 700 }}>
+      <BigCalendar
+        events={[
+          {
+            title: "My event",
+            allDay: false,
+            start: new Date(2018, 0, 1, 10, 0), // 10.00 AM
+            end: new Date(2018, 0, 1, 14, 0), // 2.00 PM
+          },
+        ]}
+        step={60}
+        view="week"
+        views={["week"]}
+        min={new Date(2008, 0, 1, 8, 0)} // 8.00 AM
+        max={new Date(2008, 0, 1, 17, 0)} // Max will be 6.00 PM!
+        date={new Date(2018, 0, 1)}
+      />
     </div>
   );
 }
-
-export default Calendar;
