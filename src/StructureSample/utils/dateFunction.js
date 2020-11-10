0451154cb3b8a586dfd01546cb306db552c7a910
Anna Stanek <aniastanekk@gmail.com>
2020-11-10 16:34:18 +0100

    Moved date format from InformationToDisplay to outside file

--- src/StructureSample/utils/dateFunction.js
+++ src/StructureSample/utils/dateFunction.js
@@ -1,3 +1,30 @@
+import React from "react";
+
+const week = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const year = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May ",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 //INPUT:2020 10 19 00 00 00
 //OUTPUT: Tue Oct 20 2020 00:00:00 GMT+0200 (Central European Summer Time)
 export function stringDateToDateFormat(userTasks) {
@@ -21,7 +48,7 @@ export function stringDateToDateFormat(userTasks) {
 //     }
 //OUTPUT: {
 //     endDate: "2020-11-6"​
-//     endTime: "24:00"
+//     endTime: "23:59"
 //     startDate: "2020-11-5"
 //     startTime: "00:00"
 //     }
@@ -33,13 +60,13 @@ export function dateFormatToObjectForInputs(range) {
     }),
     startDate: `${range.start.getFullYear()}-${
       range.start.getMonth() + 1
-    }-${range.start.getDate()}`,
+    }- ${range.start.getDate()}`,
     endTime:
       range.end.toLocaleTimeString("PL-PL", {
         hour: "numeric",
         minute: "numeric",
       }) === "00:00"
-        ? "24:00"
+        ? "23:59"
         : range.end.toLocaleTimeString("PL-PL", {
             hour: "numeric",
             minute: "numeric",
@@ -100,4 +127,45 @@ export function dateFormatToObjectForInputsWithZero(task) {
       (task.end.getMonth() + 1)
     ).slice(-2)}-${("0" + task.end.getDate()).slice(-2)}`,
   };
+}
+
+//INPUT DATE: start: Date Mon Oct 19 2020 00:00:00 GMT+0200 (Central European Summer Time)
+//            end: Date Mon Oct 19 2020 00:00:00 GMT+0200 (Central European Summer Time)
+//OUTPUT DATE version1:  1 November 00:00 - 6 November 00:00
+//OUTPUT DATE version2:  Tuesday, 12 October 14:00 - 21:00
+export function dateFormatToDisplayVersion(start, end) {
+  return (
+    <>
+      {start.toLocaleDateString() === end.toLocaleDateString() ? (
+        <>
+          {week[start.getDay()]}, {start.getDate()} {year[start.getMonth()]}
+          {"  "}
+          {start.toLocaleTimeString("PL-PL", {
+            hour: "numeric",
+            minute: "numeric",
+          })}{" "}
+          -{" "}
+          {end.toLocaleTimeString("PL-PL", {
+            hour: "numeric",
+            minute: "numeric",
+          })}
+        </>
+      ) : (
+        <>
+          {start.getDate()} {year[start.getMonth()]}
+          {"  "}
+          {start.toLocaleTimeString("PL-PL", {
+            hour: "numeric",
+            minute: "numeric",
+          })}{" "}
+          - {end.getDate()} {year[end.getMonth()]}
+          {"  "}
+          {end.toLocaleTimeString("PL-PL", {
+            hour: "numeric",
+            minute: "numeric",
+          })}
+        </>
+      )}
+    </>
+  );
 }
