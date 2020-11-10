747e7130ed287353903a71e94578ef4b211bd959
Anna Stanek <aniastanekk@gmail.com>
2020-11-10 15:17:20 +0100

    Change format date in Aside Task

--- src/StructureSample/atoms/Date.js
+++ src/StructureSample/atoms/Date.js
@@ -25,6 +25,10 @@ const year = [
   "December",
 ];
 
+//INPUT DATE: Date Mon Oct 19 2020 00:00:00 GMT+0200 (Central European Summer Time)
+//OUTPUT DATE version1:  1 November 00:00 - 6 November 00:00
+//OUTPUT DATE version2:  Tuesday, 12 October 14:00 -21:00
+
 function Date({ start, end }) {
   return (
     <>
--- src/StructureSample/pages/Aside/AsideTask/AsideTask.js
+++ src/StructureSample/pages/Aside/AsideTask/AsideTask.js
@@ -8,7 +8,7 @@ import {
   MoreInfo,
   PickedTask,
 } from "./AsideTaskStyled";
-import Date from "../../../atoms/Date";
+import DataFormater from "../../../utils/dataFormater";
 import { CSSTransition } from "react-transition-group";
 const AsideTask = ({ asideTask }) => {
   // Single Task's Data
@@ -39,12 +39,22 @@ const AsideTask = ({ asideTask }) => {
               <>
                 <PickedTask>{`→ ${asideTask.title}`}</PickedTask>
                 <MoreInfo>
-                  <p>Type: {asideTask.type}</p>
-                  <p>Description: {asideTask.description}</p>
-                  <p>Duration:</p>
-                  <Date start={asideTask.start} end={asideTask.end} />
-                  <p>Priority: {asideTask.priority}</p>
-                  <p>Project: {asideTask.project}</p>
+                  <p>
+                    <strong>Type:</strong> {asideTask.type}
+                  </p>
+                  <p>
+                    <strong>Description:</strong> {asideTask.description}
+                  </p>
+                  <p>
+                    <strong>Duration:</strong>
+                  </p>
+                  <DataFormater start={asideTask.start} end={asideTask.end} />
+                  <p>
+                    <strong>Priority:</strong> {asideTask.priority}
+                  </p>
+                  <p>
+                    <strong>Project:</strong> {asideTask.project}
+                  </p>
                 </MoreInfo>
               </>
             ) : (
